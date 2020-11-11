c88a93bad2d7c88310412af51d7f0711a709c017
MiguelGutierrez <imickey2010@hotmail.com>
2020-11-11 15:57:33 -0600

    Inicio

---
+++ recreacion.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ListSquared(m, n int) [][]int {
+	// your code
+}
+
+func main() {
+	fmt.Println(ListSquared(1, 250))
+	fmt.Println(ListSquared(42, 250))
+}
